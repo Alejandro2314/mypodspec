46110ee439294cf044c81fcc87f47fe3f0dd2f49
Alejandro Rivera <rca_230594@hotmail.com>
2018-11-28 11:40:33 -0600

    mi primer pod

---
+++ myPod/myPod.h
@@ -0,0 +1,19 @@
+//
+//  myPod.h
+//  myPod
+//
+//  Created by Gonet on 11/28/18.
+//  Copyright © 2018 Gonet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myPod.
+FOUNDATION_EXPORT double myPodVersionNumber;
+
+//! Project version string for myPod.
+FOUNDATION_EXPORT const unsigned char myPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myPod/PublicHeader.h>
+
+
